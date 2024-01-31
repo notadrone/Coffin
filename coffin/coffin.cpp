ef302d17df71ab2e61c57bfda586ba694f3d566d
Notadrone <notadrone@protonmail.com>
2024-01-31 13:21:28 -0500

    learning some things

--- coffin/coffin.cpp
+++ coffin/coffin.cpp
@@ -1,20 +1,80 @@
 // coffin.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+//https://www.openssl.org/docs/manmaster/man3/SHA256.html
+//https://www.openssl.org/docs/manmaster/man3/EVP_DigestInit_ex.html
+
+#pragma warning(disable : 4996) // need to refactor the hash functions
 
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <openssl/sha.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+std::string sha256(const std::string str) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+
+    std::stringstream ss;
+
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
+    }
+    return ss.str();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void sha256_hash_string(unsigned char hash[SHA256_DIGEST_LENGTH], char outputBuffer[65])
+{
+    int i = 0;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+
+    outputBuffer[64] = 0;
+}
+
+int sha256_file(char* path, char outputBuffer[65])
+{
+    FILE* file = fopen(path, "rb");
+    if (!file) return -534;
+
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    const int bufSize = 32768;
+    unsigned char* buffer = (unsigned char*) malloc(bufSize);
+    int bytesRead = 0;
+    if (!buffer) return ENOMEM;
+    while ((bytesRead = fread(buffer, 1, bufSize, file)))
+    {
+        SHA256_Update(&sha256, buffer, bytesRead);
+    }
+    SHA256_Final(hash, &sha256);
+
+    sha256_hash_string(hash, outputBuffer);
+    fclose(file);
+    free(buffer);
+    return 0;
+}
+int main()
+{
+    std::cout << sha256("Terminal Root") << '\n';
+    return 0;
+}
+
+//SHA256_CTX sha256;
+//SHA256_Init(&sha256);
+//FILE* f = ...; // pretend it's valid and gets cleaned up
+//while (true)
+//{
+//    unsigned char buf[4096];
+//    // in case we get less bytes back than we request
+//    size_t actual_len = read_from_file(f, buf, sizeof(buf));
+//    SHA256_Update(&sha256, buf, actual_len);
+//}
+//SHA256_Final(hash, &sha256);
